349fb49e1c0b19947472d753ad7500e3135930b6
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-11-01 12:11:22 +0200

    Update ex1.py

--- LabMD_1/ex1.py
+++ LabMD_1/ex1.py
@@ -1,16 +1,16 @@
-def print_power_set(set, set_size):
+def get_powerset(set, set_size):
     pow_set_size = 2
     for x in range(set_size-1):
         pow_set_size *= 2
-
     for counter in range(0, pow_set_size):
         sett = []
         for j in range(0, set_size):
             if counter & (1 << j):
                 sett.append(set[j])
-        print(sett)
-        print()
+        g.append(sett)
 
 
-elem = [1, 2, 3, 4, 5, 6]
-print_power_set(elem, len(elem))
+g = []
+elem = [1, 2, [1,2]]
+get_powerset(elem, len(elem))
+print(g)
