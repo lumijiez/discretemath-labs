536cb9786c4f8c730f391a71b9bbd67e2f9f2d99
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-11-03 12:34:27 +0200

    Delete bonus_psa.py

--- bonus_psa.py
+++
@@ -1,8 +0,0 @@
-import random 
-from PIL import Image
-
-img = Image.open("bonus.png").convert('RGB')
-w, h, c = img.width, img.height, 0
-for _ in range(100000):
-    c = c + 1 if img.getpixel((random.randint(0, w - 1), random.randint(0, h - 1))) == (255, 0, 0) else c
-print(c/100000 * 42)
