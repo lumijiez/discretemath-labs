973026ee1c8dc7e7c0c9793ecb8136d0f5b07e21
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-11-09 10:57:07 +0200

    Update xnor.py

--- LabMD_1/xnor.py
+++ LabMD_1/xnor.py
@@ -1,9 +1,2 @@
-def XNOR(a,b):
-    if(a == b):
-        return 1
-    else:
-        return 0
-
-x = input()
-y = input()
-print(XNOR(x,y))
+a, b = int(input()), int(input())
+print(bool((a or not b) and (not a or b)))
