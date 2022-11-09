d366260b899a3f6468eeffe890a97900cbdad979
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-11-09 09:54:52 +0200

    Update betting.py

--- LabPSA_1/betting.py
+++ LabPSA_1/betting.py
@@ -1,17 +1,13 @@
 import random
 import statistics
 
-count = 100000
 results = []
-for _ in range(count):
-    nr_success = 0
-    x = 6
-    while x > 5:
+for _ in range(100000):
+    nr_success, x = 0, 1
+    while x == 1:
         nr_success += 1
-        x = random.uniform(0, 10)
+        x = random.randint(0, 1)
     if nr_success > 1:
         results.append(pow(2, nr_success))
-results.sort()
 print(statistics.mean(results))
-
 
