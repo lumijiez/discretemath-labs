2b55db64b56b2122c7f4e82bef28c1c39689e5a1
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-12-09 12:28:58 +0200

    Update fifth.py

--- LabPSA_2/fifth.py
+++ LabPSA_2/fifth.py
@@ -4,8 +4,7 @@ from numpy import sqrt
 real = 0
 positive = 0
 for _ in range(10000):
-    b = random.uniform(-1, 1)
-    c = random.uniform(-1, 1)
+    c, b = random.uniform(-1, 1), random.uniform(-1, 1)
     delta = b ** 2 - 4*c
     if delta > 0:
         real += 1
@@ -15,5 +14,4 @@ for _ in range(10000):
             positive += 1
 print(real/10000)
 print(positive/10000)
-
 
