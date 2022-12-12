46e402215392d155205d32c0e0e35e25e3823bf4
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-12-12 16:34:01 +0200

    Update sixth.py

--- LabPSA_2/sixth.py
+++ LabPSA_2/sixth.py
@@ -1,1 +1,8 @@
 
+import random
+
+tries, wins = 1000000, 0
+for _ in range(tries):
+    if 375 <= random.randint(0, 1499) <= 1124 and 375 <= random.randint(0, 1499) <= 1124:
+        wins += 1
+print(wins/tries)
