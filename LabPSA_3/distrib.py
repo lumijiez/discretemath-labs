f8e8bc63b47cb36a086f3d05e4bc62864aca0ed4
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-12-14 12:33:51 +0200

    Update distrib.py

--- LabPSA_3/distrib.py
+++ LabPSA_3/distrib.py
@@ -1,1 +1,15 @@
 
+import random
+
+estimation = (50*200)/8
+print('estimated number of deer:' + str(estimation))
+prob_deer, deer, n = 1/25, 0, 10000
+
+for i in range(n):
+    flag = 0
+    while flag < 50:
+        deer += 1
+        if random.uniform(0, 1) <= prob_deer:
+            flag += 1
+
+print('number of deer by simulation:' + str(deer/n))
