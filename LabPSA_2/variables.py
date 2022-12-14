6b8217b50fc729d25e48d0ef4a9730dd76b767ac
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-12-14 12:24:50 +0200

    Update variables.py

--- LabPSA_2/variables.py
+++ LabPSA_2/variables.py
@@ -1,1 +1,12 @@
+import random
 
+ans, n = 0, 100000
+for i in range(n):
+    x, j = random.uniform(0, 1), 0
+    while True:
+        y = random.uniform(0, 1)
+        if y > x:
+            break
+        j += 1
+    ans += j
+print(ans/n)
