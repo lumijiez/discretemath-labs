4d3b6c293275b9e7b0b5f50a6314526dd795c585
Daniel <59575049+lemoentjiez@users.noreply.github.com>
2022-12-27 16:47:08 +0200

    Update eighth.py

--- LabPSA_2/eighth.py
+++ LabPSA_2/eighth.py
@@ -1,4 +1,3 @@
-
 import random
 
 participants, n, days = list(), 10, 10000
@@ -7,7 +6,7 @@ participants, n, days = list(), 10, 10000
 def random_list():
     l = list()
     while len(l) < n:
-        x = random.randint(1,n)
+        x = random.randint(1, n)
         if x not in l:
             l.append(x)
     return l
@@ -19,17 +18,12 @@ for i in range(n):
 ans = 0
 for i in range(days):
     lunch, dinner = random_list(), random_list()
-    # print(dinner)
-    # print(lunch)
-    for j in range(1, n+1):
+    for j in range(1, n + 1):
         idx1 = lunch.index(j)
-        # print(idx1)
         idx2 = dinner.index(j)
-        q = [lunch[idx1-1], lunch[(idx1+1) % n]]
-        w = [dinner[idx2-1], dinner[(idx2+1) % n]]
-        # print(w)
-        # print(q)
+        q = [lunch[idx1 - 1], lunch[(idx1 + 1) % n]]
+        w = [dinner[idx2 - 1], dinner[(idx2 + 1) % n]]
         if q[0] in w or q[1] in w:
             ans += 1
             break
-print(1 - ans/days)
+print(1 - ans / days)
